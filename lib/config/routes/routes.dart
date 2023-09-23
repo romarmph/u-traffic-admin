7452e2dce0da38966718c15e3109c2ad250d4173
Romar Macaraeg <roamrcm@gmail.com>
2023-09-23 13:27:17 +0800

    Add route names for main pages

--- lib/config/routes/routes.dart
+++ lib/config/routes/routes.dart
@@ -1,4 +1,10 @@
 class Routes {
   static const String home = '/';
   static const String login = '/login';
+  static const String dashboard = '/analytics';
+  static const String tickets = '/tickets';
+  static const String enforcers = '/enforcers';
+  static const String adminStaffs = '/admin-staffs';
+  static const String system = '/system';
+  static const String settings = '/settings';
 }
