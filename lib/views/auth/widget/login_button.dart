f7a6c2cbb0ee822433731caa5dec803e0f0b912f
Romar Macaraeg <roamrcm@gmail.com>
2023-10-28 22:01:53 +0800

    Update

--- lib/views/auth/widget/login_button.dart
+++ lib/views/auth/widget/login_button.dart
@@ -73,7 +73,7 @@ class LoginButton extends ConsumerWidget {
       }
     }
 
-    return ElevatedButton(
+    return UElevatedButton(
       onPressed: login,
       child: isLoading
           ? const CircularProgressIndicator(
